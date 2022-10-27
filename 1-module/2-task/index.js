fc34763f3172f1b23119d505b5719003379d6bc3
Oksana Sergeeva <oksanasergeeva@Oksanas-MacBook-Pro.local>
2022-10-27 20:15:47 +0300

    name task done

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -1,24 +1,32 @@
 /**
  * Эту функцию трогать не нужно
  */
-function print(text) {
+ function print(text) {
   console.log(text);
 }
 
 /**
- * Эту функцию нужно поменять так,
- * чтобы функция sayHello работала корректно
- */
+* Эту функцию нужно поменять так,
+* чтобы функция sayHello работала корректно
+*/
 function isValid(name) {
-  // ваш код...
+  if (name !== null && typeof name !== 'undefined') {
+      if (name.length >= 4 && !name.includes(' ')) {
+          return true;
+      } else {
+          return false;
+      }
+  } else {
+      return false;
+  }
 }
 
 function sayHello() {
-  let userName = prompt('Введите ваше имя');
+  let userName = prompt("Введите ваше имя");
 
   if (isValid(userName)) {
-    print(`Welcome back, ${userName}!`);
+      print(`Welcome back, ${userName}!`);
   } else {
-    print('Некорректное имя');
+      print("Некорректное имя");
   }
 }
