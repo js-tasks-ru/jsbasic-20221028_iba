c927f01d966429bcf02fdc89358f3b716c52b0e9
Oksana Sergeeva <oksanasergeeva@Oksanas-MacBook-Pro.local>
2022-11-11 23:46:19 +0200

    2 1-task done

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,10 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let salariesSumm = 0;
+  Object.values(salaries).forEach((salary) => {
+    
+    if (typeof salary === 'number' && !isNaN(salary) && salary !== Infinity && salary !== -Infinity) {
+      salariesSumm += salary
+    }
+  });
+  return salariesSumm
 }
