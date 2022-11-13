d53917322e2aa0ec998d15f8884a4209b139f064
Ilana Sandler <oksanasergeeva@Ilanas-MacBook-Pro.local>
2022-11-13 14:36:18 +0200

    2-3 fixes

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,19 +1,13 @@
-function read(a, b) {
-  return this['a'] = a, this['b'] = b
-}
-
-function sum(a, b) {
-  return this.a + this.b;
-}
-
-function mul(a, b) {
- return this.a * this.b;
-}
-
 let calculator = {
-  read,
-  sum,
-  mul,
+  read(a, b) {
+    return this['a'] = a, this['b'] = b
+  },
+  sum(a, b) {
+    return this.a + this.b;
+  },
+  mul(a, b) {
+    return this.a * this.b;
+   },
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
