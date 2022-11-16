cda6a830d163ec4b167c6ab7fa0da3a4ec5e3b22
Ilana Sandler <oksanasergeeva@Ilanas-MacBook-Pro.local>
2022-11-16 21:35:53 +0200

    3-5 done

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,12 @@
 function getMinMax(str) {
-  // ваш код...
+    let result = {
+        min: 0,
+        max: 0,
+    };
+    let arr = str.split(' ').filter(i => parseFloat(i)).sort((a,b) => a - b);
+    result.min = parseFloat(arr[0]);
+    result.max = parseFloat(arr[arr.length - 1]);
+
+
+    return result
 }
