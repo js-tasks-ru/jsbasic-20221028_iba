897869e1aa361fa975d4abe4466a8ff262420f5c
Ilana Sandler <oksanasergeeva@Ilanas-MacBook-Pro.local>
2022-11-18 20:04:54 +0200

    4-2 done

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,13 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  const rows = Array.from(table.rows);
+
+  rows.forEach((row, rowNumber) => {
+    const cells = Array.from(row.cells);
+
+    cells.forEach((cell, cellNumber) => {
+      if (rowNumber === cellNumber) {
+        cell.style.backgroundColor = 'red';
+      }
+    });
+  });
 }
