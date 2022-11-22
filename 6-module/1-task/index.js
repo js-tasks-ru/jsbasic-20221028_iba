8b32301b21749355438ad2904c7904229715ae7f
Ilana Sandler <oksanasergeeva@Ilanas-MacBook-Pro.local>
2022-11-22 22:16:34 +0200

    6-1 first (wrong mostly) version)

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,58 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+  }
+
+  createRow(tbody) {
+    this.rows.forEach(row => {
+      // линия tr
+      const tableLine = document.createElement('tr');
+      const removeButton = `<button type="button">X</button>`;
+      tbody.append(tableLine);
+      row.button = removeButton;
+
+      // ячейки td
+      this.createCell(row, tableLine);
+    });
+  }
+
+  createCell(row, tableLine) {
+    // ячейки td
+    Object.values(row).forEach(cell => {
+      const tableCell = document.createElement('td');
+      tableLine.append(tableCell);
+      tableCell.innerHTML = cell;
+    });
+  }
+
+  remove(tbody) {
+    tbody.addEventListener('click', function(event) {
+      event.preventDefault();
+      if (event.target.tagName === 'BUTTON') {
+        event.target.parentNode.parentNode.remove();
+      }
+    })
+  }
+
+  elem() {
+    const tableLayout = document.createElement('table');
+    const tableHead = `
+    <thead>
+        <tr>
+            <th>Имя</th>
+            <th>Возраст</th>
+            <th>Зарплата</th>
+            <th>Город</th>
+            <th></th>
+        </tr>
+    </thead>`;
+    tableLayout.innerHTML = tableHead;
+    const tbody = document.createElement('tbody');
+    document.body.append(tableLayout);
+    tableLayout.append(tbody);
+
+    this.createRow(tbody);
+    this.remove(tbody);
   }
 }
