3ece3dd7228822db401362eff0cfe16b8e80942a
Ilana Sandler <9151o66926@gmail.com>
2022-11-29 22:16:36 +0200

    7-1 done

--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -8,12 +8,12 @@ export default class RibbonMenu {
 
   get render() {
     this.elem = createElement(`<div class="ribbon">
-    <button class="ribbon__arrow ribbon__arrow_left ribbon__arrow_visible">
+    <button class="ribbon__arrow ribbon__arrow_left">
       <img src="/assets/images/icons/angle-icon.svg" alt="icon">
     </button>
     <nav class="ribbon__inner">
     </nav>
-    <button class="ribbon__arrow ribbon__arrow_right">
+    <button class="ribbon__arrow ribbon__arrow_right ribbon__arrow_visible">
       <img src="/assets/images/icons/angle-icon.svg" alt="icon">
     </button>
     </div>`); 
@@ -32,6 +32,7 @@ export default class RibbonMenu {
     });
 
     this.elem.addEventListener('click', this.onClick);
+    ribbonInner.addEventListener('scroll', this.onScroll);
 
     return this.elem
   }
@@ -52,5 +53,44 @@ export default class RibbonMenu {
       });
       event.target.dispatchEvent(newEvent);
     }
+
+    // работа прокрутки
+    if (event.target.classList.contains('ribbon__arrow') || event.target.parentElement.classList.contains('ribbon__arrow')) {
+      const carouselArrowLeft = event.target.classList.contains('ribbon__arrow_left');
+      const carouselArrowImgLeft = event.target.offsetParent.classList.contains('ribbon__arrow_left');
+      const carouselArrowRight = event.target.classList.contains('ribbon__arrow_right');
+      const carouselArrowImgRight = event.target.offsetParent.classList.contains('ribbon__arrow_right');
+      let ribbonInner = document.querySelector('.ribbon__inner');
+
+      // проверка нажатия именно на кнопки назад
+      if (carouselArrowLeft || carouselArrowImgLeft) {
+        ribbonInner.scrollBy(-350, 0)
+      }
+      // проверка нажатия именно на кнопки вперед
+      if (carouselArrowRight || carouselArrowImgRight) {
+        ribbonInner.scrollBy(350, 0)
+      }
+    }
+  }
+
+  onScroll(event) {
+      const btnLeft = document.querySelector('.ribbon__arrow_left');
+      const btnRight = document.querySelector('.ribbon__arrow_right');
+      const ribbonLinks = ribbonInner.querySelectorAll('.ribbon__item');
+      
+      // скрывать кнопки
+      // показать кнопку "налево" на втором и последующих слайдах
+      if (event.target.scrollLeft > ribbonLinks[0].offsetWidth) {
+        btnLeft.classList.add('ribbon__arrow_visible');
+      } else {
+        btnLeft.classList.remove('ribbon__arrow_visible');
+      }
+
+      // скрыть кнопку "направо" на последнем слайде
+      if (event.target.scrollLeft >= event.target.offsetWidth / 2) {
+        btnRight.classList.remove('ribbon__arrow_visible');
+      } else {
+        btnRight.classList.add('ribbon__arrow_visible');
+      }
   }
 }
