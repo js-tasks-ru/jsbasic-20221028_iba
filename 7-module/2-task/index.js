1e0fe255aeecfff974791633d77c565eb142da90
Ilana Sandler <9151o66926@gmail.com>
2022-12-04 22:29:26 +0200

    7-2 done?

--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -2,16 +2,13 @@ import createElement from '../../assets/lib/create-element.js';
 
 export default class Modal {
   constructor() {
-    this.popup = this.popup;
+    this.popup = this._createPopup;
     this.title = this.title;
     this.body = this.body;
   }
 
-  _createPopup() {
-    this.popup = document.createElement('div');
-    this.popup.classList.add('modal');
-
-    this.popup.innerHTML = `<div class="modal">
+  get _createPopup() {
+    this.popup = createElement(`<div class="modal">
     <!--Прозрачная подложка перекрывающая интерфейс-->
     <div class="modal__overlay"></div>
 
@@ -24,7 +21,8 @@ export default class Modal {
       </div>
     </div>
 
-  </div>`;
+  </div>
+    `)
    
     return this.popup
   }
@@ -47,7 +45,7 @@ export default class Modal {
 
   open() {
     const page = document.body;
-    page.append(this._createPopup());
+    page.append(this.popup);
     const modalHeader = this.popup.querySelector('.modal__header');
     const modalInner = this.popup.querySelector('.modal__inner');
     modalHeader.append(this.title);
@@ -61,13 +59,12 @@ export default class Modal {
       }
     });
     modalInner.addEventListener('click', this.close);
-    return this.popup
   }
 
   close(event) {
     if (event.target.closest('.modal__close') || event.target.offsetParent.closest('.modal__close')) {
       const page = document.body;
-      const popup = event.target.closest('div.modal'); // при `.remove()` удаляет содержимое внутри, но не сам элемент
+      const popup = event.target.closest('div.modal');
    
       page.classList.remove('is-modal-open');
       popup.remove();
