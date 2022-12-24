65f0114398c4058293ffb88679da8845e815b598
Ilana Sandler <9151o66926@gmail.com>
2022-12-24 23:21:56 +0200

    8-2 done

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -5,11 +5,11 @@ export default class ProductGrid {
   constructor(products) {
     this.products = products;
     this.filters = {};
-    this.elem = this._createElem;
-    this.productsInner = this.elem.querySelector('.products-grid__inner');
-    this.updateFilter = this.updateFilter;
+    this.elem = this.#createElem();
+    this.#showCards();
   }
-  get _createElem() {
+  
+  #createElem() {
     //const card = new ProductCard(this.products);
     const grid = createElement(`<div class="products-grid"><div class="products-grid__inner"></div></div>`);
     const productsInner = grid.querySelector('.products-grid__inner');
@@ -22,32 +22,38 @@ export default class ProductGrid {
     return  grid
   }
 
-  _appendProducts() {
-    this.products.forEach((card) => {
-      let cardClass = new ProductCard(card);
-      this.productsInner.append(cardClass.elem);
-    });
-  }
-
-  updateFilter(obj) {
-    const pageCards = Array.from(this.elem.querySelectorAll('.card'));
-    this.productsInner.innerHTML = '';
-
-
-    if (!event.currentTarget.checked) {
-      this._appendProducts();
-    } else {
-      pageCards.forEach(card => {
-        console.log(card.dataset)
-        if (
-          obj.category && card.dataset.category && obj.category === card.dataset.category ||
-          obj.maxSpiciness && card.maxSpiciness && card.maxSpiciness <= obj.maxSpiciness ||
-          obj.vegeterianOnly && card.vegeterianOnly ||
-          obj.noNuts && card.noNuts
-          ) {
-          this.productsInner.append(card);
+  #showCards() {
+    let filteredProducts = this.products;
+    if (this.filters) {
+      for(const [key, value] of Object.entries(this.filters)) {
+        if (key === 'noNuts' && value === true) {
+          filteredProducts = filteredProducts.filter((item) => item['nuts'] !== value);
         }
-      });
+        if (key === 'vegeterianOnly' && value === true) {
+          filteredProducts = filteredProducts.filter((item) => item['vegeterian'] === value);
+        }
+        if (key === 'maxSpiciness') {
+          filteredProducts = filteredProducts.filter((item) => item['spiciness'] <= value);
+        }
+        if (key === 'category' && value)  {
+          filteredProducts = filteredProducts.filter((item) => item[key] === value);
+        }
+
+      }
+
     }
+    const holder = this.elem.querySelector('.products-grid__inner');
+    holder.innerHTML = '';
+    for (const product of filteredProducts) {
+      let card = new ProductCard(product);
+      holder.append(card.elem);
+    };
+
   }
+
+  updateFilter(filters) {
+    this.filters = Object.assign(this.filters, filters);
+    this.#showCards();
+  }
+ 
 }
